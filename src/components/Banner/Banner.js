5b390b0fcd4f93895eaca17ff9871996776d5e3e
mehedifoysal <mehedifoysal8@gmail.com>
2022-04-04 05:50:29 +0600

    banner component complete

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,27 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './Banner.css';
 import {Link} from "react-router-dom";
-import BannerImage from './images/banner.jpg';
+import {Col, Container, Row} from "react-bootstrap";
 
 const Banner = () => {
     const [banner, setBanner] = useState([]);
+    useEffect(() => {
+        fetch('data/banner.json')
+            .then(res => res.json())
+            .then(data => setBanner(data))
+    }, []);
 
     return (
         <section className="ic-banner-section">
-            <div className="container">
-                <div className="ic-banner-content">
-                    <div className="row align-items-center">
-                        <div className="col-lg-6 col-md-8 col-sm-12">
-                            <h1 className="ic-banner-title">Pick Your Best Choice</h1>
-                            <p>Apple revealed the new 14- and 16- inch MacBook Pro laptops at its “Unleashed” event. The new laptop features a new redesign with smaller bezels, a new display, new ports, new Apple silicon, and more.</p>
-                            <Link to="/reviews" className="ic-custom-btn-rvs ic-mt-60">View Details</Link>
-                        </div>
-                        <div className="col-lg-6 col-md-4 col-sm-12">
-                            <img src={BannerImage} alt=""/>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Row className="ic-banner-content align-items-center">
+                    <Col lg={6} md={6} sm={12}>
+                        <h1 className="ic-banner-title">{banner.bannerTitle}</h1>
+                        <p>{banner.bannerDescription}</p>
+                        <Link to="/reviews" className="btn-default">{banner.bannerButtonText}</Link>
+                    </Col>
+                    <Col lg={6} md={6} sm={12}>
+                        <img src={banner.bannerImage} alt="" className='img-fluid'/>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     );
 };
