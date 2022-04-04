1dacd77a1b2d870f48d3149ca4029fdb78da5573
mehedifoysal <mehedifoysal8@gmail.com>
2022-04-05 01:35:17 +0600

    404 page design

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './NotFound.css';
+import {Container} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <div className="pp-404-section text-center">
+            <Container>
+                <div className="pp-404-text">
+                    <div className="pp-title">
+                        <h2>404</h2>
+                        <div className="pp-error">
+                            <p>error</p>
+                        </div>
+                    </div>
+
+                    <div className="pp-404-btn">
+                        <h4>Sorry!</h4>
+                        <p>The Page You Are Looking For Was Not Found.</p>
+                        <Link to={'/'} className="btn-default">Back To Home</Link>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
