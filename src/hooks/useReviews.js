0630914b639c784530b63dca702325ed9164f7a2
mehedifoysal <mehedifoysal8@gmail.com>
2022-04-04 22:24:05 +0600

    useReviews update

--- src/hooks/useReviews.js
+++ src/hooks/useReviews.js
@@ -2,21 +2,11 @@ import {useEffect, useState} from "react";
 
 const useReviews = () => {
     const [reviews, setReviews] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
-        setLoading(true);
         fetch('data/reviewData.json')
             .then(response => response.json())
-            .then(data => {
-                setReviews(data);
-                setLoading(false);
-            })
-            .catch(error => {
-                setError(error);
-                setLoading(false);
-            });
+            .then(data => setReviews(data))
     }, []);
 
     return [reviews, setReviews];
