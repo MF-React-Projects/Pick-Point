e8ee2548be2381d70a2e0208cc705a1d6f995862
mehedifoysal <mehedifoysal8@gmail.com>
2022-04-04 21:09:28 +0600

    mobile menu create and header design update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,39 +4,57 @@ import {Link} from "react-router-dom";
 import {Container, Nav} from "react-bootstrap";
 
 const Header = () => {
+
+    const handleMobileMenuIn = () => {
+        document.querySelector('.header').classList.remove('nav-open');
+        document.querySelector('.header').style.transitionDelay = '0.5s';
+        document.querySelector('.header .layer').style.transitionDelay = '0.3s';
+        document.querySelector('.header .inner').style.transitionDelay = '0s';
+        document.querySelector('.hamburger.hamburger-in').classList.remove('opened-navi');
+    }
+
+    const handleMobileMenuOut = () => {
+        document.querySelector('.header').classList.add('nav-open');
+        document.querySelector('.header.nav-open').style.transitionDelay = '0s';
+        document.querySelector('.header.nav-open .layer').style.transitionDelay = '0.2s';
+        document.querySelector('.header.nav-open .inner').style.transitionDelay = '0.7s';
+        document.querySelector('.hamburger.hamburger-in').classList.add('is-opened-navi');
+    }
+
     return (
-        <header className="ic-header">
-            <div className="ic-header-bottom">
-                <div className="ic-menu-wrapper">
-                    <Nav className="ic-primary-menu">
+        <header className="header">
+            <div className="header-bottom">
+                <div className="menu-wrapper">
+                    <Nav className="primary-menu">
                         <Container>
-                            <div className="ic-menu-area">
-                                <div className="ci-header-logo">
+                            <div className="menu-area">
+                                <div className="header-logo">
                                     <Link to="/">
                                         <h4>Pick Point</h4>
                                     </Link>
                                 </div>
-                                <div className="ic-main-menu-area">
-                                    <ul className="ic-main-menu">
-                                        <li className="ic-menu-item active">
+                                <div className="main-menu-area">
+                                    <ul className="main-menu">
+                                        <li className="menu-item active">
                                             <Link to="/">Home</Link>
                                         </li>
-                                        <li className="ic-menu-item">
+                                        <li className="menu-item">
                                             <Link to='/reviews'>Reviews</Link>
                                         </li>
-                                        <li className="ic-menu-item">
+                                        <li className="menu-item">
                                             <Link to="/dashboard">Dashboard</Link>
                                         </li>
-                                        <li className="ic-menu-item">
+                                        <li className="menu-item">
                                             <Link to="/blogs">Blogs</Link>
                                         </li>
-                                        <li className="ic-menu-item">
+                                        <li className="menu-item">
                                             <Link to="/about">About</Link>
                                         </li>
                                     </ul>
-                                    <div className="ic-nav-icon">
-                                        <div className="ic-nav-icon-inner">
-                                            <div className="hamburger hamburger-out" id="hamburger">
+                                    <div className="nav-icon">
+                                        <div className="nav-icon-inner">
+                                            <div onClick={handleMobileMenuOut} className="hamburger hamburger-out"
+                                                 id="hamburger">
                                                 <div className="hamburger__line hamburger__line--01">
                                                     <div className="hamburger__line-in hamburger__line-in--01"></div>
                                                 </div>
@@ -64,29 +82,29 @@ const Header = () => {
                     </Nav>
                 </div>
             </div>
-            <div className="ic-mobile-menu-main">
-                <div className="ic-mobile-menu">
-                    <ul className="ic-main-menu d-flex flex-column">
-                        <li className="ic-menu-item active">
+            <div className="mobile-menu-main">
+                <div className="mobile-menu">
+                    <ul className="main-menu d-flex flex-column">
+                        <li className="menu-item active">
                             <Link to="/">Home</Link>
                         </li>
-                        <li className="ic-menu-item">
+                        <li className="menu-item">
                             <Link to='/reviews'>Reviews</Link>
                         </li>
-                        <li className="ic-menu-item">
+                        <li className="menu-item">
                             <Link to="/dashboard">Dashboard</Link>
                         </li>
-                        <li className="ic-menu-item">
+                        <li className="menu-item">
                             <Link to="/blogs">Blogs</Link>
                         </li>
-                        <li className="ic-menu-item">
+                        <li className="menu-item">
                             <Link to="/about">About</Link>
                         </li>
                     </ul>
-                    <div className="ic-nav-burger">
-                        <div className="ic-nav-icon">
-                            <div className="ic-nav-icon-inner">
-                                <div className="hamburger hamburger-in is-opened-navi" id="hamburger">
+                    <div className="nav-burger">
+                        <div className="nav-icon">
+                            <div className="nav-icon-inner">
+                                <div onClick={handleMobileMenuIn} className="hamburger hamburger-in is-opened-navi" id="hamburger">
                                     <div className="hamburger__line hamburger__line--01">
                                         <div className="hamburger__line-in hamburger__line-in--01"></div>
                                     </div>
